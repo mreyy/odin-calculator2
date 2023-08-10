fcf1a21b51b0786217050d56c45157ba7091aff1
Marilyn <65829768+mreyy@users.noreply.github.com>
2023-08-10 02:52:50 -0700

    Add files via upload

---
+++ odin-calculator2/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const digits = document.querySelectorAll(".digits");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+const equals = document.querySelector(".equals");
+
+console.log(digits);
+
